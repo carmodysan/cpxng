d88ddd7738923ef7bf2741391c0d43ab5dedfccb
carmodysan <carmodysan@outlookcom>
2020-11-29 22:36:34 +0100

    Make the view greater !

--- controllers/maincontroller.py
+++ controllers/maincontroller.py
@@ -9,7 +9,9 @@ class Controller():
         self.root = Tk.Tk()
         self.model = mainmodel.Model()
         self.view = mainview.View(self.root)
-        self.view.sidepanel.label.bind("<Button-1>", self.quit)
+        self.view.sidepanel.label_quit.bind("<Button-1>", self.quit)
+        self.view.sidepanel.label_quit.bind("<Enter>", self.mouse_enter_quit)
+        self.view.sidepanel.label_quit.bind("<Leave>", self.mouse_leave_quit)
 
     def run(self):
         self.root.title("Cartes CPx")
@@ -18,3 +20,11 @@ class Controller():
 
     def quit(self, event):
         self.root.destroy()
+
+    def mouse_enter_quit(self, event):
+        self.view.sidepanel.label_quit.configure(image=self.view.sidepanel.eimg_quit_active, cursor='hand2')
+        self.view.sidepanel.label_quit.image = self.view.sidepanel.eimg_quit_active
+
+    def mouse_leave_quit(self, event):
+        self.view.sidepanel.label_quit.configure(image=self.view.sidepanel.eimg_quit, cursor='arrow')
+        self.view.sidepanel.label_quit.image = self.view.sidepanel.eimg_quit
--- views/mainview.py
+++ views/mainview.py
@@ -1,7 +1,6 @@
 from PIL import Image, ImageTk
 import tkinter as Tk
-
-import fontawesome as fa
+from sys import platform
 
 from utils import observer
 from views import menu
@@ -12,20 +11,32 @@ from views import menu
 class View():
     def __init__(self, master):
         self.frame = Tk.Frame(master)
-        master.attributes('-zoomed', True)
 
-        # Définit le menu principal
-        self.menu = menu.Menu(master)
-
-        self.logo = Logo(master)
-        self.sidepanel = SidePanel(master)
-        self.top_toolbar = TopToolBar(master)
-
+        # Check operating system to maximize window correctly
+        if platform == 'linux':
+            master.attributes('-zoomed', True)  # For Linux
+        elif platform == 'win32' or platform == 'cygwin':
+            master.state('zoomed')  # For Windows
 
         # Définit la barre d'état
         self.statusbar = StatusBar(master)
         self.statusbar.observe(
             'update_connect_db', self.statusbar.update_connect_db)
+
+        # The left frame contains logo and possible actions
+        self.left_frame = Tk.Frame(master)
+        self.logo = Logo(self.left_frame)
+        self.sidepanel = SidePanel(self.left_frame)
+        self.left_frame.pack(side=Tk.LEFT, fill=Tk.Y, anchor=Tk.NW)
+
+        # The top frame contains the toolbar
+        self.top_toolbar = TopToolBar(master)
+
+        # The main frame contains the content
+        self.main_frame = Tk.Frame(master)
+        content = Tk.Label(self.main_frame, text="My super content !")
+        content.pack(side=Tk.TOP, fill=Tk.BOTH, expand=Tk.YES)
+        self.main_frame.pack(side=Tk.TOP, fill=Tk.BOTH, anchor=Tk.CENTER)
 
 
 # Classe définissant la barre d'état
@@ -42,16 +53,19 @@ class StatusBar(observer.Observer):
 
 class SidePanel():
     def __init__(self, root):
-        self.frame_side_panel = Tk.Frame(root, bg='#91c46b') #Police : #517a3e
+        self.frame_side_panel = Tk.Frame(root, bd=1, bg='#91c46b')
 
-        self.img = Image.open("ressources/sign-out-alt.png").resize((32, 32))
-        self.eimg = ImageTk.PhotoImage(self.img)
+        # Quit module
+        self.img_quit = Image.open("ressources/sign-out-alt.png").resize((32, 32))
+        self.eimg_quit = ImageTk.PhotoImage(self.img_quit)
+        self.img_quit_active = Image.open("ressources/sign-out-alt-active.png").resize((32, 32))
+        self.eimg_quit_active = ImageTk.PhotoImage(self.img_quit_active)
 
-        self.label = Tk.Label(self.frame_side_panel, image=self.eimg, bg='#91c46b')
-        self.label.image = self.eimg
-        self.label.pack(side=Tk.TOP, padx=10, pady=10)
+        self.label_quit = Tk.Label(self.frame_side_panel, image=self.eimg_quit, bg='#91c46b')
+        self.label_quit.image = self.eimg_quit
+        self.label_quit.pack(side=Tk.TOP, padx=10, pady=10)
 
-        self.frame_side_panel.pack(side=Tk.LEFT, fill=Tk.Y)
+        self.frame_side_panel.pack(side=Tk.TOP, fill=Tk.Y, expand=Tk.YES)
 
         #self.frame_side_panel.pack(side=Tk.LEFT, fill=Tk.Y, expand=1)
         #self.button_about = Tk.Button(self.frame_side_panel, text='?')
@@ -63,18 +77,18 @@ class TopToolBar():
     def __init__(self, root):
         self.frame_top_tb = Tk.Frame(root, bg='#34383c') #Police : #6f7176
 
-        self.img = Image.open("ressources/exit.png")
+        self.img = Image.open("ressources/sign-out-alt.png").resize((32, 32))
         eimg = ImageTk.PhotoImage(self.img)
 
-        exitButton = Tk.Button(self.frame_top_tb, image=eimg, relief=Tk.FLAT, width='40')
-        exitButton.image = eimg
-        exitButton.pack(side=Tk.LEFT, padx=2, pady=2)
+        label = Tk.Label(self.frame_top_tb, image=eimg, bg='#34383c')
+        label.image = eimg
+        label.pack(side=Tk.LEFT, padx=10, pady=11)
 
-        self.frame_top_tb.pack(side=Tk.TOP, fill=Tk.X)
+        self.frame_top_tb.pack(side=Tk.TOP, anchor=Tk.NW, fill=Tk.X)
 
 class Logo():
     def __init__(self, root):
-        self.frame_logo = Tk.Frame(root, bg='#1d2226')
+        self.frame_logo = Tk.Frame(root, bd=1, bg='#1d2226')
 
         self.img_logo = Image.open("ressources/logo.png").resize((32, 32))
         self.eimg_logo = ImageTk.PhotoImage(self.img_logo)
@@ -83,4 +97,4 @@ class Logo():
         label = Tk.Label(self.frame_logo, image=self.eimg_logo, bg='#1d2226')
         label.pack(side=Tk.LEFT, padx=10, pady=10)
 
-        self.frame_logo.pack(side=Tk.LEFT, fill=Tk.NONE, anchor=Tk.NW)
+        self.frame_logo.pack(side=Tk.TOP, anchor=Tk.NW)
